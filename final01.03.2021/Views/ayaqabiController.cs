179252df77a7689eda754c0c1ac8958e6631e6b7
huseyn222 <sosyalmedya150@gmail.com>
2021-03-08 21:03:56 +0400

    huseyn543

---
+++ final01.03.2021/Views/ayaqabiController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace final01._03._2021.Views
+{
+    public class ayaqabiController : Controller
+    {
+        // GET: ayaqabi
+        public ActionResult ayaqabi()
+        {
+            return View();
+        }
+    }
+}
---
+++ final01.03.2021/Views/womanController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace final01._03._2021.Views
+{
+    public class womanController : Controller
+    {
+        // GET: woman
+        public ActionResult woman()
+        {
+            return View();
+        }
+    }
+}
